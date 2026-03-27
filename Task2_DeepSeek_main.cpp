7555559999a0ad07cb9aff05553d25de45868875
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-27 18:55:03 +0500

    Create Task2_DeepSeek_main.cpp

---
+++ Task2_DeepSeek_main.cpp
@@ -0,0 +1,11 @@
+#include "head_Task2_DeepSeek.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Выполняем запуск Task2_DeepSeek" << std::endl;
+    Task2_DeepSeek();
+  
+    return 0;
+}
