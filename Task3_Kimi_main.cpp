32dcc5810e45d0c03825043c33d9f60a278765c5
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-27 19:08:06 +0500

    Create Task3_Kimi_main.cpp

---
+++ Task3_Kimi_main.cpp
@@ -0,0 +1,11 @@
+#include "head_Task3_Kimi.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Выполняем запуск Task3_Kimi" << std::endl;
+    Task3_Kimi();
+  
+    return 0;
+}
