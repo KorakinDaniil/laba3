5c9bba5bbbff06b6c0b409bef0f05a7818bc837c
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-27 18:49:09 +0500

    Create head_Task1_Gemini.h

---
+++ head_Task1_Gemini.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <cctype>
+#include <map>
+#include <functional>
+#include <stack>
+
+int Task1_Gemini();
