e8807817e1029818c3b98fcba71ba5337ee2fcda
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-27 18:51:35 +0500

    Create Task1_Gemini_main.cpp

---
+++ Task1_Gemini_main.cpp
@@ -0,0 +1,11 @@
+#include "head_Task1_Gemini.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Выполняем запуск Task1_Gemini" << std::endl;
+    Task1_Gemini();
+  
+    return 0;
+}
