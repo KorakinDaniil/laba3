c4118690a798e1c89fd768e9f530903d54fb8a92
KorakinDaniil <korakindaniil66@gmail.com>
2026-03-27 19:08:43 +0500

    Create head_Task3_Gemini.h

---
+++ head_Task3_Gemini.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <cctype>
+#include <map>
+#include <functional>
+#include <stack>
+
+//FIX_ME: using namespace std; запрещено стандартом
+//using namespace std;
+
+int Task3_Gemini();
